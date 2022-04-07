d97bbfbead6b2476ac69327dfe839ccdce14fc99
Corwin Van Deusen <35018264+swirty@users.noreply.github.com>
2022-04-07 19:28:52 -0400

    refactored to use ncurses

--- snake1.c
+++ snake1.c
@@ -4,10 +4,37 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<termios.h>
-int headX,headY,width=25,height=25,gameOver=0,direction=2,size=4,score=0,speed=100000;
+#include <ncurses.h>
+
+int headX,headY,width=25,height=25,gameOver=0,direction=2,size=5,score=0,speed=100000;
 int bodyX[30],bodyY[30];
 
-int kbhit(void) //kbhit is only on Windows, so I looked up the code to implement into Linux based C code
+void updateSnake(void);
+void setupGame(void);
+void inputCheck(void);
+void moveSnake(void);
+
+int main(){
+  initscr();
+  clear();
+  cbreak();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
+  noecho();
+  setupGame();
+  while(!gameOver){
+    inputCheck();
+    moveSnake();
+    updateSnake();
+    refresh();
+    usleep(speed);
+  }
+  usleep(speed);
+  system("clear");
+  endwin();
+}
+
+/*int kbhit(void) //kbhit is only on Windows, so I looked up the code to implement into Linux based C code
 {
   struct termios oldt, newt;
   int ch;
@@ -32,82 +59,61 @@ int kbhit(void) //kbhit is only on Windows, so I looked up the code to implement
   }
 
   return 0;
-}
-char getch(void) //Also a Windows method, had to look up literal code to implement into this project, gets user inputs in terms of up,down,left,right keys
-{
-    char c;
-    system("stty raw");
-    c= getchar();
-    system("stty sane");
-    return(c);
-}
-
-void create()
-{
- system("clear");
- int i,j,k,p;
- for(j=0;j<=height;j++)
- {
-  for(i=0;i<=width;i++)
-  {
-    p=1;
-    if(i==0||i==width)
-      printf("|");
-    else if(j==0||j==height)
-        printf("-");
-    else if(i==headX&&j==headY)
-      printf("@");
-    else
-    {
-     for(k=0;k<size;k++)
-     {
-      if(i==bodyX[k] && j==bodyY[k])
-      {
-       printf("o");
-       p=0;
-      }
-     }
-      if(p)
-      printf(" ");
-    }
-  }
-  printf("\n");
- }
- printf("Scored : %d",score);
-}
+}*/
 
 void setupGame()
 {
- headX=5;
- headY=2;
+  headX=5;
+  headY=2;
+
+  int i,j;
+  addch('#');
+  hline('-', width-1);
+  move(0, width);
+  addch('#');
+  move(1,0);
+  vline('|', height-1);
+  move(1, width);
+  vline('|', height-1);
+  move(height,0);
+  addch('#');
+  hline('-', width-1);
+  move(height, width);
+  addch('#');
+  refresh();
 }
 
-void inputCheck()
-{
- if(kbhit())
- {
-  char key=getch();
-  switch (key)
-  {
-   case 'w':
-    direction=1;
-    break;
-   case 'd':
-    direction=2;
-    break;
-   case 's':
-    direction=3;
-    break;
-   case 'a':
-    direction=4;
+void inputCheck(){
+  switch (getch()){
+    case 'w':
+    case KEY_UP:
+      direction=1;
+      break;
+
+    case 'd':
+    case KEY_RIGHT:
+      direction=2;
+      break;
+
+    case 's':
+    case KEY_DOWN:
+      direction=3;
+      break;
+
+    case 'a':
+    case KEY_LEFT:
+      direction=4;
+      break;
+
+    case ERR:
+      break;
   }
- }
 }
 
 void moveSnake()
 {
  int x1,x2,y1,y2,i;
- 
+
   x1=headX;
   y1=headY;
   for(i=0;i<size;i++)
@@ -119,7 +125,7 @@ void moveSnake()
    x1=x2;
    y1=y2;
   }
- 
+
  switch (direction)
  {
    case 1:
@@ -136,15 +142,18 @@ void moveSnake()
  }
 }
 
-int main()
-{
-    setupGame();
-    while(!gameOver){
-        create();
-        inputCheck();
-        moveSnake();
-        usleep(speed);
-    }    
-    usleep(speed);
-    system("clear");
-}
+ void updateSnake(void){
+   int i=0;
+
+   for(i=1; i<width; i++){
+     move(1,i);
+     vline(' ', height-1);
+   }
+
+   for(i=0; i<size; i++){
+     move(bodyY[i], bodyX[i]);
+     addch('o');
+   }
+   move(bodyY[i], bodyX[i]);
+   addch('@');
+ }
