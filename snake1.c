c069fde7e96c5450f50b9f7dc5d3a51ddc169e68
Corwin Van Deusen <35018264+swirty@users.noreply.github.com>
2022-04-13 12:14:35 -0400

    Still bugged, but mostly complete.

--- snake1.c
+++ snake1.c
@@ -1,113 +1,94 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<termios.h>
-int headX,headY,width=25,height=25,gameOver=0,direction=2,size=4,score=0,speed=100000;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ncurses.h>
+#include <string.h>
+#include <signal.h>
+
+int headX,headY,width,height,gameOver=0,direction=2,size=4,score=0,speed=100000;
 int bodyX[30],bodyY[30];
 
-int kbhit(void) //kbhit is only on Windows, so I looked up the code to implement into Linux based C code
-{
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
+void updateSnake(void);
+void setupGame(void);
+void inputCheck(void);
+void moveSnake(void);
+void exitGame(int);
 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
+int main(){
+  initscr();
+  clear();
+  width = COLS-1;
+  height = LINES-1;
+  cbreak();
+  keypad(stdscr, TRUE);
+  nodelay(stdscr, TRUE);
+  curs_set(0);
+  noecho();
+  setupGame();
+  while(!gameOver){
+    inputCheck();
+    moveSnake();
+    updateSnake();
+    refresh();
+    usleep(speed);
+    signal(SIGINT, exitGame);
   }
-
-  return 0;
-}
-char getch(void) //Also a Windows method, had to look up literal code to implement into this project, gets user inputs in terms of up,down,left,right keys
-{
-    char c;
-    system("stty raw");
-    c= getchar();
-    system("stty sane");
-    return(c);
-}
-
-void create()
-{
- system("clear");
- int i,j,k,p;
- for(j=0;j<=height;j++)
- {
-  for(i=0;i<=width;i++)
-  {
-    p=1;
-    if(i==0||i==width)
-      printf("|");
-    else if(j==0||j==height)
-        printf("-");
-    else if(i==headX&&j==headY)
-      printf("@");
-    else
-    {
-     for(k=0;k<size;k++)
-     {
-      if(i==bodyX[k] && j==bodyY[k])
-      {
-       printf("o");
-       p=0;
-      }
-     }
-      if(p)
-      printf(" ");
-    }
-  }
-  printf("\n");
- }
- printf("Scored : %d",score);
 }
 
 void setupGame()
 {
- headX=5;
- headY=2;
+  headX=5;
+  headY=2;
+
+  int i,j;
+  addch('#');
+  hline('-', width-1);
+  move(0, width);
+  addch('#');
+  move(1,0);
+  vline('|', height-1);
+  move(1, width);
+  vline('|', height-1);
+  move(height,0);
+  addch('#');
+  hline('-', width-1);
+  move(height, width);
+  addch('#');
+  refresh();
 }
 
-void inputCheck()
-{
- if(kbhit())
- {
-  char key=getch();
-  switch (key)
-  {
-   case 'w':
-    direction=1;
-    break;
-   case 'd':
-    direction=2;
-    break;
-   case 's':
-    direction=3;
-    break;
-   case 'a':
-    direction=4;
+void inputCheck(){
+  switch (getch()){
+    case 'w':
+    case KEY_UP:
+      direction=1;
+      break;
+
+    case 'd':
+    case KEY_RIGHT:
+      direction=2;
+      break;
+
+    case 's':
+    case KEY_DOWN:
+      direction=3;
+      break;
+
+    case 'a':
+    case KEY_LEFT:
+      direction=4;
+      break;
+
+    case ERR:
+      break;
   }
- }
 }
 
 void moveSnake()
 {
  int x1,x2,y1,y2,i;
- 
+
   x1=headX;
   y1=headY;
   for(i=0;i<size;i++)
@@ -119,7 +100,7 @@ void moveSnake()
    x1=x2;
    y1=y2;
   }
- 
+
  switch (direction)
  {
    case 1:
@@ -136,15 +117,26 @@ void moveSnake()
  }
 }
 
-int main()
-{
-    setupGame();
-    while(!gameOver){
-        create();
-        inputCheck();
-        moveSnake();
-        usleep(speed);
-    }    
-    usleep(speed);
-    system("clear");
-}
+ void updateSnake(void){
+   for(int i=1; i<width; i++){
+     move(1,i);
+     vline(' ', height-1);
+   }
+
+   for(int i=0; i<size; i++){
+     move(bodyY[i], bodyX[i]);
+     printw("%i", i);
+   }
+   move(headY, headX);
+   addch('@');
+ }
+
+ void exitGame(int a){
+   char *endtext = "Exiting";
+   move(height/2, (width - strlen(endtext)) / 2);
+   printw("%s", endtext);
+   refresh();
+   usleep(speed * 4);
+   endwin();
+   exit(a);
+ }
